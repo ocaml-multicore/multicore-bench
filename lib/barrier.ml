aeffdbe94c0dc8138fea03789421465e4e89538c
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-02-27 22:36:56 +0200

    Auto resetting single word barrier

--- lib/barrier.ml
+++ lib/barrier.ml
@@ -1,13 +1,26 @@
-type t = { counter : int Atomic.t; total : int }
+(** This barrier is designed to take a single cache line (or word) and to return
+    with the participating domains synchronized as precisely as possible. *)
+
+type t = int Atomic.t
+
+let bits = (Sys.int_size - 1) / 2
+let mask = (1 lsl bits) - 1
+let one = 1 lsl bits
 
 let make total =
-  { counter = Atomic.make 0 |> Multicore_magic.copy_as_padded; total }
-  |> Multicore_magic.copy_as_padded
+  if total <= 0 || mask < total then invalid_arg "Barrier: out of bounds";
+  Atomic.make total |> Multicore_magic.copy_as_padded
 
-let await { counter; total } =
-  if Atomic.get counter = total then
-    Atomic.compare_and_set counter total 0 |> ignore;
-  Atomic.incr counter;
-  while Atomic.get counter < total do
+let await t =
+  let state = Atomic.fetch_and_add t one in
+  let total = state land mask in
+  if state lsr bits = total - 1 then Atomic.set t (total - (total lsl bits));
+
+  while 0 < Atomic.get t do
+    Domain.cpu_relax ()
+  done;
+
+  Atomic.fetch_and_add t one |> ignore;
+  while Atomic.get t < 0 do
     Domain.cpu_relax ()
   done
