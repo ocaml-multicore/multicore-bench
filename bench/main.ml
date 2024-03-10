9e9303d4faf5d0954b4d67b71e94095c82810855
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-03-10 09:54:35 +0200

    Add benchmarks of some `Stdlib` data structures

---
+++ bench/main.ml
@@ -0,0 +1,10 @@
+let benchmarks =
+  [
+    ("Ref with [@poll error]", Bench_ref.run_suite);
+    ("Ref with Mutex", Bench_ref_mutex.run_suite);
+    ("Atomic", Bench_atomic.run_suite);
+    ("Queue", Bench_queue.run_suite);
+    ("Stack", Bench_stack.run_suite);
+  ]
+
+let () = Multicore_bench.Cmd.run ~benchmarks ()
