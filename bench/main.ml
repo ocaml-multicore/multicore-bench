27862f23ac437b7be503c0a4ff1f4307e34a9476
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-04-16 18:40:26 +0300

    Add `Hashtbl` benchmark

--- bench/main.ml
+++ bench/main.ml
@@ -3,6 +3,7 @@ let benchmarks =
     ("Ref with [@poll error]", Bench_ref.run_suite);
     ("Ref with Mutex", Bench_ref_mutex.run_suite);
     ("Atomic", Bench_atomic.run_suite);
+    ("Hashtbl", Bench_hashtbl.run_suite);
     ("Queue", Bench_queue.run_suite);
     ("Stack", Bench_stack.run_suite);
     ("Unix", Bench_unix.run_suite);
