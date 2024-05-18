afb53727fd8ef40c60abdaca6e7d0cc199f63892
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-05-18 18:02:34 +0300

    Add `Metric.make` to support ad hoc metrics

--- lib/metric.ml
+++ lib/metric.ml
@@ -1,1 +1,20 @@
 type t = Yojson.Safe.t
+
+let to_nonbreaking s =
+  s |> String.split_on_char ' ' |> String.concat " " (* a non-breaking space *)
+
+let name ~metric ~config = to_nonbreaking (metric ^ "/" ^ config)
+
+let make ~metric ~config ~value ?units ?trend ?description () =
+  let[@inline] ( @: ) x_opt xs =
+    match x_opt with None -> xs | Some x -> x :: xs
+  in
+  `Assoc
+    (Some ("name", `String (name ~metric ~config))
+    @: Some ("value", `Float value)
+    @: Option.map (fun units -> ("units", `String units)) units
+    @: Option.map (fun trend -> ("trend", Trend.to_json trend)) trend
+    @: Option.map
+         (fun description -> ("description", `String description))
+         description
+    @: [])
